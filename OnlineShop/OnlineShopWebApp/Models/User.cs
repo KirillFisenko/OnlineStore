961fcf7e879d05b1ce307f97beda3302249a1b1d
KirillFisenko <79265463469@ya.ru>
2023-07-11 14:28:40 +0300

    #

--- OnlineShop/OnlineShopWebApp/Models/User.cs
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -2,15 +2,12 @@
 {
     public class User
     {
-        private static int instanceCounter = 1;
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public static List<Product> ShoppingList { get; set; }
+        public Guid Id { get; set; }        
+        public string Name { get; set; }        
         public User(string name)
         {
-            Name = name;
-            Id = instanceCounter;
-            instanceCounter++;
+            Name = name;   
+            Id = Guid.NewGuid();
         }
     }
 }
