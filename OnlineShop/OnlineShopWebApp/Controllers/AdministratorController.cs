abc9da0b3130d5993ac7be6c7c6885bb33596cf7
KirillFisenko <79265463469@ya.ru>
2023-07-30 21:20:58 +0300

    #

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdministratorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdministratorController : Controller
+    {        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetOrders()
+        {
+            var orders = "Заказы";
+            return View();
+        }
+
+        public IActionResult GetUsers()
+        {
+            return View("Пользователи");
+        }
+
+        public IActionResult GetRoles()
+        {
+            return View("Роли");
+        }
+
+        public IActionResult GetProducts()
+        {
+            return View("Продукты");
+        }
+
+    }
+}
