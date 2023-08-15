b708dcb2d04603cf0e5ed62dd1d47cabfc117474
KirillFisenko <79265463469@ya.ru>
2023-08-15 13:10:20 +0300

    #

--- OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
@@ -4,12 +4,12 @@ using OnlineShopWebApp.Models;
 namespace OnlineShopWebApp.Controllers
 {
     public class OrderController : Controller
-    {        
+    {
         private readonly ICartsRepository cartsRepository;
         private readonly IOrdersRepository ordersRepository;
 
         public OrderController(ICartsRepository cartsRepository, IOrdersRepository ordersRepository)
-        {            
+        {
             this.cartsRepository = cartsRepository;
             this.ordersRepository = ordersRepository;
         }
@@ -21,12 +21,17 @@ namespace OnlineShopWebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Buy(Order order)
-        {            
-            order.Cart = cartsRepository.TryGetByUserId(Constants.UserId);
+        public IActionResult Buy(UserDeliveryInfo user)
+        {
+            var existingCart = cartsRepository.TryGetByUserId(Constants.UserId);
+            var order = new Order
+            {
+                User = user,
+                Items = existingCart.Items
+            };
             ordersRepository.Add(order);
             cartsRepository.Clear(Constants.UserId);
             return View(order);
-        }        
+        }
     }
 }
---
+++ OnlineShop/OnlineShopWebApp/Models/UserDeliveryInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class UserDeliveryInfo
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
