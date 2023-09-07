ec6c2ba359b1b6cd4c4218698cf76a8cadfa7e56
KirillFisenko <79265463469@ya.ru>
2023-09-07 18:03:58 +0300

    Подключение БД MS SQL

---
+++ OnlineShop/OnlineShop.Db/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Db.Models
+{
+	public class Product
+	{	
+		public Guid Id { get; set; }
+        
+        public string Name { get; set; }
+      	
+		public decimal Cost { get; set; }
+
+		public string Description { get; set; }
+  
+		public string ImagePath { get; set; }		
+	}
+}
