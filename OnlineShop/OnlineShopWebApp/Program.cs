d39f6e6b27909f2a6cf53d7905d0f6be5d20caac
KirillFisenko <79265463469@ya.ru>
2023-10-19 17:19:04 +0300

    добавил StaticFileOptions

--- OnlineShop/OnlineShopWebApp/Program.cs
+++ OnlineShop/OnlineShopWebApp/Program.cs
@@ -30,13 +30,13 @@ builder.Services.AddAutoMapper(typeof(MappingProfile));
 // настройка параметров локализации запросов
 builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
-	var supportedCultures = new[]
-	{
-		new CultureInfo("en-US")
-	};
-	options.DefaultRequestCulture = new RequestCulture("en-US");
-	options.SupportedCultures = supportedCultures;
-	options.SupportedUICultures = supportedCultures;
+    var supportedCultures = new[]
+    {
+        new CultureInfo("en-US")
+    };
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
 });
 
 // получаем строку подключения из файла конфигурации
@@ -50,19 +50,19 @@ builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlServer(c
 
 // указываем тип пользователя и роли
 builder.Services.AddIdentity<User, IdentityRole>()
-				// устанавливаем тип хранилища - наш контекст
-				.AddEntityFrameworkStores<IdentityContext>();
+                // устанавливаем тип хранилища - наш контекст
+                .AddEntityFrameworkStores<IdentityContext>();
 
 // настройка cookie
 builder.Services.ConfigureApplicationCookie(options =>
 {
-	options.ExpireTimeSpan = TimeSpan.FromHours(8);
-	options.LoginPath = "/Account/Login";
-	options.LogoutPath = "/Account/Logout";
-	options.Cookie = new CookieBuilder
-	{
-		IsEssential = true
-	};
+    options.ExpireTimeSpan = TimeSpan.FromHours(8);
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.Cookie = new CookieBuilder
+    {
+        IsEssential = true
+    };
 });
 
 // создание экземпляра приложения
@@ -71,11 +71,11 @@ var app = builder.Build();
 // если приложение не находится в режиме разработки
 if (!app.Environment.IsDevelopment())
 {
-	// подключение обработчика исключений
-	app.UseExceptionHandler("/Home/Error");
+    // подключение обработчика исключений
+    app.UseExceptionHandler("/Home/Error");
 
-	// подключение HSTS
-	app.UseHsts();
+    // подключение HSTS
+    app.UseHsts();
 }
 
 // подключение локализации запросов
@@ -85,7 +85,13 @@ app.UseRequestLocalization();
 app.UseSerilogRequestLogging();
 
 // подключение статических файлов
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions()
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Add("Cache-Control", "public,max-age=600");
+    }
+});
 
 // подключение маршрутизации
 app.UseRouting();
@@ -98,13 +104,13 @@ app.UseAuthorization();
 
 // определение маршрута контроллера для area
 app.MapControllerRoute(
-	name: "MyArea",
-	pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    name: "MyArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 // определение маршрута по умолчанию
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 // инициализация администратора
 using (var serviceScope = app.Services.CreateScope())
