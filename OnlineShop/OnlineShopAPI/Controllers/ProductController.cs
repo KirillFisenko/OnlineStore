02ea814f83310506c62af8071a053a0199e1046a
KirillFisenko <79265463469@ya.ru>
2023-10-31 15:24:26 +0300

    удалить

---
+++ OnlineShop/OnlineShopAPI/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    // контроллер продукта
+    public class ProductController : Controller
+    {
+		private readonly IProductsRepository productsRepository;
+		private readonly IMapper mapper;
+
+		public ProductController(IProductsRepository productsRepository, IMapper mapper)
+		{
+			this.productsRepository = productsRepository;
+			this.mapper = mapper;
+		}
+
+		// отображение продукта
+		public async Task<IActionResult> Index(Guid productId)
+		{
+            var product = await productsRepository.TryGetByIdAsync(productId);
+			var model = mapper.Map<ProductViewModel>(product);
+			return View(model);
+		}        
+    }
+}
---
+++ OnlineShop/Test/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using OnlineShop.Db;
+using OnlineShopWebApp.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductsRepository productsRepository;
+        private readonly IMapper mapper;
+
+        public HomeController(IProductsRepository productsRepository, IMapper mapper)
+        {
+            this.productsRepository = productsRepository;
+            this.mapper = mapper;
+        }
+
+        // отображение всех продуктов
+        [HttpGet("Index")]
+        public async Task<List<ProductViewModel>> Index()
+        {
+            var products = await productsRepository.GetAllAsync();
+            var model = products.Select(mapper.Map<ProductViewModel>).ToList();
+            return model;
+        }
+    }
+}
